8be5774fc009bef46510cc01aad40653ad31a079
Marynel Vazquez <marynel.vazquez@yale.edu>
2020-10-25 23:11:48 -0400

    added extra python dep

--- assignment-5/install_python_deps.sh
+++ assignment-5/install_python_deps.sh
@@ -1,2 +1,2 @@
-python -m pip install -U --user numpy tensorflow==2.0.0b1 tensorboard==2.1.0 scipy matplotlib scikit-learn gdown
+python -m pip install -U --user numpy tensorflow==2.0.0b1 tensorboard==2.1.0 scipy matplotlib scikit-learn gdown urdf-parser-py
 sudo apt-get install python-tk  # required by tensorflow
