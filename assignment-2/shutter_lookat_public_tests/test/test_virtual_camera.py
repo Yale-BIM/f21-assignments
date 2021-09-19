1abd97ab5ea93fc54d53e2d2c80c8369d628e8d4
Marynel Vázquez <marynel.vazquez@yale.edu>
2021-09-19 11:05:11 -0400

    Update test_virtual_camera.py

--- assignment-2/shutter_lookat_public_tests/test/test_virtual_camera.py
+++ assignment-2/shutter_lookat_public_tests/test/test_virtual_camera.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python2.7
-# Public tests for CPSC459/559 Assignment 3 - Part III
+#!/usr/bin/env python3
+# Public tests for CPSC459/559 Assignment 2 - Part III
 
 PKG = "shutter_lookat_public_tests"
 NAME = 'test_virtual_camera'
