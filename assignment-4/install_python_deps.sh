4305f3586749307530518aa6bd2e624145773379
Marynel Vázquez <marynel.vazquez@yale.edu>
2021-10-18 16:22:54 -0400

    Update install_python_deps.sh

--- assignment-4/install_python_deps.sh
+++ assignment-4/install_python_deps.sh
@@ -2,7 +2,7 @@
 pip3 install --user -U pip
 
 # install tensorflow and other python deps
-pip3 install -U --user numpy tensorflow==2.6.0 tensorboard==2.7.0 scipy matplotlib scikit-learn gdown
+pip3 install -U --user tensorflow==2.6.0 tensorboard==2.7.0 scipy matplotlib scikit-learn gdown
 
 # install additional deps
 sudo apt-get install python3-tk  # required by tensorflow
